30c1c3794eac5a873615dd5bae46d363ff8e2064
Hebron George <hebrontgeorge@gmail.com>
2020-02-05 23:51:05 -0600

    helper method to get all the broken videos in the playlist snapshot

--- app/models/playlist_snapshot.rb
+++ app/models/playlist_snapshot.rb
@@ -1,14 +1,15 @@
 require 'yt'
 
 class PlaylistSnapshot < ApplicationRecord
+  BROKEN_STATUSES = ['Deleted video', 'Private video']
+
   # attr_accessor :playlist_id, :channel_id, :playlist_items
   def self.create_snapshot!
     playlist_id = 'FL7B_s7wxX-D__fkTiYp3Oaw'
     channel_id  = 'UC7B_s7wxX-D__fkTiYp3Oaw'
 
     all_songs = Yt::Playlist.new(id: playlist_id).playlist_items.where;
-    song_count = all_songs.count
-    playlist_items = {song_count: song_count}
+    playlist_items = {}
     all_songs.each do |song|
       song_id = song.snippet.data['resourceId']['videoId']
       playlist_items[song_id] = song.snippet.data
@@ -19,5 +20,11 @@ class PlaylistSnapshot < ApplicationRecord
     ps.channel_id  = channel_id
     ps.playlist_items = playlist_items
     ps.save!
+  end
+
+  def broken_songs
+    playlist_items.select do |_song_id, song|
+      BROKEN_STATUSES.include?(song['title'])
+    end
   end
 end
