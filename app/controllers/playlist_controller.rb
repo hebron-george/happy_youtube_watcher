afe0bf4e179d0a8d50db04974703e9a03d786593
Hebron George <hebrontgeorge@gmail.com>
2020-02-08 15:24:11 -0600

    init commit; wip

---
+++ app/controllers/playlist_controller.rb
@@ -0,0 +1,8 @@
+class PlaylistController < ApplicationController
+  # <iframe src='https://www.youtube.com/embed/<%= @episode.youtube_id %>?rel=0&autoplay=<%= params[:autoplay] || 0 %>' frameborder='0' allowfullscreen></iframe>
+
+  # Shuffle's a given youtube playlist for you
+  def shuffle
+
+  end
+end
