89b608e0a9889b23a427be58de1a225ecea1f305
Hebron George <hebrontgeorge@gmail.com>
2020-02-08 17:28:24 -0600

    WIP

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,4 +1,4 @@
-class PlaylistController < ApplicationController
+class PlaylistsController < ApplicationController
   # <iframe src='https://www.youtube.com/embed/<%= @episode.youtube_id %>?rel=0&autoplay=<%= params[:autoplay] || 0 %>' frameborder='0' allowfullscreen></iframe>
 
   # Shuffle's a given youtube playlist for you
