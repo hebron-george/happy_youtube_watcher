35e19e759f5a5a116464689a17fdd755d5794b11
Hebron George <hebrontgeorge@gmail.com>
2020-03-18 21:30:36 -0500

    Remove debug statement

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -3,7 +3,6 @@ class PlaylistsController < ApplicationController
 
   # Shuffle's a given youtube playlist for you
   def shuffle
-    puts "At the beginning of the shuffle action!"
     playlist_ids = params[:playlist_ids]&.split(',')
     @playlists = PlaylistSnapshot.where(playlist_id: playlist_ids)
     json_response({error_message: 'Could not find the playlist(s)!', playlist_id: params[:playlist_id]},:not_found) and return unless @playlists
