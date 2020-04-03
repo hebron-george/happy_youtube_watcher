eb913a1e57d781f7a58e7842659a0431585239af
Hebron George <hebrontgeorge@gmail.com>
2020-04-02 21:00:47 -0500

    Add Name to TrackedPlaylist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_220926) do
+ActiveRecord::Schema.define(version: 2020_04_03_020015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,9 @@ ActiveRecord::Schema.define(version: 2020_04_02_220926) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_default"
+    t.string "name"
     t.index ["is_default"], name: "index_tracked_playlists_on_is_default"
+    t.index ["name"], name: "index_tracked_playlists_on_name"
   end
 
 end
