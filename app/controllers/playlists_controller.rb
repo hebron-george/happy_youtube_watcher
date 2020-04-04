044d0375a5751f189da5db77985c4d2e25df6de4
ras1 <ras2@live.com>
2020-04-03 22:43:46 -0500

    Update shuffle to return default when IDs are blank

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -3,7 +3,7 @@ class PlaylistsController < ApplicationController
 
   # Shuffle's a given youtube playlist for you
   def shuffle
-    playlist_ids = params[:playlist_ids]
+    playlist_ids = params[:playlist_ids].blank? ? TrackedPlaylist.where(:is_default => true).pluck(:playlist_id) : params[:playlist_ids]
     shuffled_video_list = {
       songs: PlaylistSnapshot.shuffle_playlists(playlist_ids),
     }
