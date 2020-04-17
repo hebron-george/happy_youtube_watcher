09544a38b9da8943e237e805b4958c480b725b44
Hebron George <hebrontgeorge@gmail.com>
2020-04-16 21:05:17 -0500

    Sort by playlist name

--- app/controllers/tracked_playlists_controller.rb
+++ app/controllers/tracked_playlists_controller.rb
@@ -1,6 +1,6 @@
 class TrackedPlaylistsController < ApplicationController
   def index
-    tps = TrackedPlaylist.all.map { |tp| {playlist_id: tp.playlist_id, name: tp.name, is_default: tp.is_default} }
+    tps = TrackedPlaylist.all.map { |tp| {playlist_id: tp.playlist_id, name: tp.name, is_default: tp.is_default} }.sort_by { |tp| tp[:name] }
     json_response(tps)
   end
 
