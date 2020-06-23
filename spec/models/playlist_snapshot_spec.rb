034b1727088532c7ebf83ce3d2a7bcd7ec51afc9
Hebron George <hebrontgeorge@gmail.com>
2020-06-22 19:32:59 -0500

    initial test for .calculate_diffs

--- spec/models/playlist_snapshot_spec.rb
+++ spec/models/playlist_snapshot_spec.rb
@@ -1,6 +1,7 @@
+require 'rails_helper'
 require 'rspec'
 
-describe 'PlaylistSnapshot' do
+describe PlaylistSnapshot do
   before do
     # Do nothing
   end
@@ -9,9 +10,30 @@ describe 'PlaylistSnapshot' do
     # Do nothing
   end
 
-  context 'when condition' do
+  context '.calculate_diffs' do
+    subject { PlaylistSnapshot.calculate_diffs(current_songs, previous_songs) }
+
+    let(:current_songs) do
+      {
+        song_id_1: {song_info: {name: 'song1'}},
+        song_id_2: {song_info: {name: 'song2'}},
+        song_id_3: {song_info: {name: 'hayramloozrs'}},
+      }
+    end
+
+    let(:previous_songs) do
+      {
+        song_id_1: {song_info: {name: 'song1'}},
+        song_id_2: {song_info: {name: 'song2'}},
+        song_id_4: {song_info: {name: 'ayylmao'}},
+      }
+    end
+
+    let(:expected_removed) { [{song_info: {name: 'ayylmao'}}] }
+    let(:expected_added)   { [{song_info: {name: 'hayramloozrs'}}] }
+
     it 'succeeds' do
-      pending 'Not implemented'
+      expect(subject).to eq({added: expected_added, removed: expected_removed})
     end
   end
 end
