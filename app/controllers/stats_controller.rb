21df62f3b4962d32284dd033bfcf117678141018
Hebron George <hebrontgeorge@gmail.com>
2021-01-21 22:52:12 -0500

    Songs per playlist pie chart

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,11 @@
 class StatsController < ApplicationController
   def index
+    @songs_in_playlists = songs_in_playlists
   end
 
+  private
+
+  def songs_in_playlists
+    TrackedPlaylist.all.inject({}) { |acc, tp| acc[tp.name] = tp.playlist_snapshots.newest.working_songs.count; acc }
+  end
 end
