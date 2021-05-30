8ac16e7a8b92a17825761b5f7963e0ecd4923303
Ras2 <ras2@live.com>
2021-05-30 00:35:34 -0500

    Move channel_id to tracked_playlists

--- db/migrate/20210530051457_move_channel_id_to_tracked_playlists.rb
+++ db/migrate/20210530051457_move_channel_id_to_tracked_playlists.rb
@@ -1,8 +1,5 @@
 class MoveChannelIdToTrackedPlaylists < ActiveRecord::Migration[6.0]
   def change
-    add_column :tracked_playlists, :channel_id, :string
-    add_index :tracked_playlists, :channel_id
-
     execute "UPDATE tracked_playlists t, playlist_snapshots s SET t.channel_id = s.channel_id WHERE t.playlist_id = s.playlist_id"
 
     remove_column :playlist_snapshots, :channel_id, :string
